ffb575ca49e3efa4d5739658c7a8e3ee56faed4c
rodman01 <email4joern@arcor.de>
2017-01-15 09:45:51 +0100

    Revert "smdk4412: new cam hal"
    This reverts commit bcdc01d0cf40a5bed2dbb3265b50011d3f0b26b2.

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,46 +1,41 @@
-ifneq ($(TARGET_PROVIDES_CAMERA_HAL),true)
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
-# HAL module implemenation stored in
-# hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.product.board>.so
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_SRC_FILES := \
+	SecCameraHWInterface.cpp
 
-LOCAL_C_INCLUDES += \
-	hardware/samsung/exynos4/hal/include \
-	system/media/camera/include
+LOCAL_SHARED_LIBRARIES := libutils libcutils liblog libcamera_client libhardware
+LOCAL_SHARED_LIBRARIES := libhwjpeg libfimc libsecion
 
-LOCAL_SRC_FILES:= \
-	SecCamera.cpp SecCameraHWInterface.cpp
-
-LOCAL_SHARED_LIBRARIES:= libutils libcutils libbinder liblog libcamera_client libhardware
-
-ifeq ($(TARGET_SOC), exynos4210)
-LOCAL_SHARED_LIBRARIES += libs5pjpeg
-LOCAL_CFLAGS += -DSAMSUNG_EXYNOS4210
-endif
-
-ifeq ($(TARGET_SOC), exynos4x12)
-LOCAL_SHARED_LIBRARIES += libhwjpeg
-LOCAL_CFLAGS += -DSAMSUNG_EXYNOS4x12
-endif
-
-ifeq ($(BOARD_USE_V4L2), true)
-LOCAL_CFLAGS += -DBOARD_USE_V4L2
-endif
-
-ifeq ($(BOARD_USE_V4L2_ION), true)
-LOCAL_CFLAGS += -DBOARD_USE_V4L2
-LOCAL_CFLAGS += -DBOARD_USE_V4L2_ION
-endif
+LOCAL_C_INCLUDES := \
+	system/media/camera/include \
+	hardware/samsung/exynos4/hal/include
 
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/media
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
-LOCAL_MODULE := camera.$(TARGET_BOOTLOADER_BOARD_NAME)
+LOCAL_PRELINK_MODULE := false
 
+LOCAL_MODULE := camera.$(TARGET_BOOTLOADER_BOARD_NAME)
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_CFLAGS += -O0
@@ -48,5 +43,3 @@ LOCAL_CFLAGS += -O0
 LOCAL_CLANG := false
 
 include $(BUILD_SHARED_LIBRARY)
-
-endif
